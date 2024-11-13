8398178dd7915d7f44646f27aff7882ed676efae
ash4670 <ash4670@yahoo.com>
2024-11-13 17:10:10 +0200

    newradzenmenu

---
+++ Shared/GlobalVars.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nrcv2.Shared
+{
+    public static class GlobalVars
+    {
+        public static int AppId { get; set; } = 2;//1 stock//2 ohda //3 cost //purch
+    }
+}
