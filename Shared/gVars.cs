4816b6ba2013600a3ed7faac8fcd961c604ee0b2
ash4670 <ash4670@yahoo.com>
2024-11-26 23:33:04 +0200

    loginadjusted

--- Shared/gVars.cs
+++ Shared/gVars.cs
@@ -8,5 +8,6 @@ namespace nrcv2.Shared
     public static class Gvars
     {
         public static int AppId { get; set; } = 1;//1 stock//2 ohda //3 cost //purch
-     }
+        public static string guername { get; set; } = "";
+    }
 }
